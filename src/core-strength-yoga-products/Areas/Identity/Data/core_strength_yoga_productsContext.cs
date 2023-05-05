337eb64ab3520cce7b2c6d3ba8dcde0bb0ca4bed
Susan Traynor <traynosm@gmail.com>
2023-05-05 20:54:52 +0100

    add identity

---
+++ src/core-strength-yoga-products/Areas/Identity/Data/core_strength_yoga_productsContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace core_strength_yoga_products.Data;
+
+public class core_strength_yoga_productsContext : IdentityDbContext<IdentityUser>
+{
+    public core_strength_yoga_productsContext(DbContextOptions<core_strength_yoga_productsContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
---
+++ src/core-strength-yoga-products/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace core_strength_yoga_products.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
