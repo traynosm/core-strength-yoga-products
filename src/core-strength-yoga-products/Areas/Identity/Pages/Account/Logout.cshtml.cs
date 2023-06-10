6763628747c2ed7f8a183d8c3a0c559cabca12e4
dmalone98 <109853007+dmalone98@users.noreply.github.com>
2023-06-10 14:11:57 +0100

    updated payment form css

--- src/core-strength-yoga-products/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ src/core-strength-yoga-products/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -33,13 +33,13 @@ namespace core_strength_yoga_products.Areas.Identity.Pages.Account
            // _logger.LogInformation("User logged out.");
            // if (returnUrl != null)
             //{
-                return LocalRedirect("/");
+                return LocalRedirect("Index");
             //}
             //else
             //{
                 // This needs to be a redirect so that the browser performs a new
                 // request and the identity for the user gets updated.
-                return RedirectToPage();
+                return RedirectToPage("Index");
             //}
         }
     }
